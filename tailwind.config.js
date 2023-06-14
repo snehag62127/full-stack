4878b89d88de3eba9489d319768ecf49820c6ef5
snehag62127 <118705053+snehag62127@users.noreply.github.com>
2023-06-14 20:09:01 +0530

    updated readme file and added css file

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    './pages/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#fff'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      slackside:['slackside One',"cursive"]
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    theme: ["light","cupcake","dark"],
+  }
+}
+
